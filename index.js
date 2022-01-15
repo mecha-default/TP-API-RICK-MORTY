da5cc592be4e48d1fadaa31c4f60e3bc87a398d8
Mercedes <mercedes.fresno@gmail.com>
2022-01-15 20:40:35 -0300

    modifico archivos sass

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ fetch ("")
 .then ((data)=>{
 console.log(data)
 })
+
+//https://rickandmortyapi.com/api/character
+//https://rickandmortyapi.com/api/location
+//https://rickandmortyapi.com/api/episode
