500330104f231578ee1419cf5f160fd5f8b2580e
Mercedes <mercedes.fresno@gmail.com>
2022-01-19 20:20:46 -0300

    Implementacion Paginado

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 
-// LINK PRESONAJES//
+
+const baseUrl ="https://rickandmortyapi.com/api/"
+
+
+// SECCION RESULTADOS
+const sectionResultadosPersonajes= document.querySelector (".section-resultados-personajes")
+const sectionResultadosEpisodios= document.querySelector (".section-resultados-episodios")
+const sectionResultadosUbicaciones= document.querySelector (".section-resultados-ubicaciones")
+const sectionDetallePersonaje= document.querySelector(".section-detalle-personaje")
+
+//PAGINADO
+let paginaActual = 1
+let ultimaPagina = 0
+const botonPrevPersonajes = document.querySelector(".prev-personajes")
+const botonNextPersonajes = document.querySelector(".next-personajes")
+
+
+// LINK PERSONAJES//
 const linkPersonajes = document.getElementById("personajes")
+linkPersonajes.onclick = () => {
+    sectionResultadosPersonajes.style.display = "flex";
+    sectionResultadosEpisodios.style.display = "none";
+    sectionResultadosUbicaciones= "none";
+    sectionDetallePersonaje.style.display = "none";  
+    
+}
 const mostrarPersonajes = ()=>{
-    fetch("https://rickandmortyapi.com/api/character?page=1")
+    fetch(`${baseUrl}character?page=${paginaActual}`)
     .then(res => res.json())
     .then(data =>{
-        mostrarPersonajeEnHTML(data.results)
+        mostrarPersonajesEnHTML(data.results)
     })
 }
 mostrarPersonajes()
 
-const tarjetaPersonaje = document.querySelector(".seccion-personaje")
-const mostrarPersonajeEnHTML = (array) => {
+const mostrarPersonajesEnHTML = (array) => {
     const html = array.reduce((acc,curr)=>{
       return acc = acc + 
       `<div class="card" data-id=${curr.id}>
@@ -20,14 +43,35 @@ const mostrarPersonajeEnHTML = (array) => {
      </div>`
     },"")
 
-    tarjetaPersonaje.innerHTML = html
-    
+// IMPLEMENTACION PAGINADO //
+   if(paginaActual === 1){
+        botonPrevPersonajes.disabled = true}
+        else{
+            botonPrevPersonajes.disabled = false
+        }
+
+    sectionResultadosPersonajes.innerHTML = html  
 }  
 
-/*
-const baseUrl ="https://rickandmortyapi.com/api/"
+botonPrevPersonajes.onclick = () => {
+    paginaActual--
+    if (paginaActual === 1){
+        botonPrevPersonajes.disabled = true
+    }
+    else{
+        botonPrevPersonajes.disabled = false
+    }
+    mostrarPersonajes()
+}
+botonNextPersonajes.onclick = () => {
+    paginaActual++
+    if(paginaActual === ultimaPagina){
+        botonNextPersonajes.disabled = true
+    }
+    mostrarPersonajes()
 
-*/
+}
+
 /*
 fetch ("")
 .then (res=>res.json()))
@@ -39,3 +83,10 @@ console.log(data)
 //https://rickandmortyapi.com/api/character
 //https://rickandmortyapi.com/api/location
 //https://rickandmortyapi.com/api/episode
+
+/*
+sectionResultadosPersonajes.style.display= none
+sectionResultadosEpisodios.style.display= none
+sectionResultadosUbicaciones.style.display= none
+sectionDetallePersonaje.style.display= none
+*/
